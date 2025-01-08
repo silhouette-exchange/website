df7b629424f748519e8e2bfa15324e939dca7fd3
Stenton Mayne <stentonian@protonmail.com>
2025-01-08 16:02:28 +0200

    Change site logo & title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import './App.css'
 
 function App() {
     const [count, setCount] = useState(0)
-    console.log("HELLO", silhouetteLogo)
 
     const divStyle = {
         backgroundImage: `url(${silhouetteLogo})`,
