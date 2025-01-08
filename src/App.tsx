ca691a718e6ebce01708350ec888ccd8bb7d41f9
Stenton Mayne <stentonian@protonmail.com>
2025-01-08 14:48:41 +0200

    [hack] Image as landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import silhouetteLogo from '/landing_page.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    console.log("HELLO", silhouetteLogo)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const divStyle = {
+        backgroundImage: `url(${silhouetteLogo})`,
+        backgroundPosition: "left",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+    };
+
+    return (
+        <>
+            <div className="cComponent" style={divStyle} />
+            {/* <div>
+                <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+                </div>
+                <h1>Vite + React</h1>
+                <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+                </button>
+                <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+                </div>
+                <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+                </p> */}
+        </>
+    )
 }
 
 export default App
