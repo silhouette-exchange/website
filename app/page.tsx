eafe5eec4fbbd2b0fb0f92d9138a72163aa4d60d
setup <setup@example.com>
2025-01-08 19:39:04 +0000

    Attempt to fix css mess when deploying to gh-pages
    Followed some advice here https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
    
    Also changed background color & image a tad

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,34 @@ import Footer from './components/Footer'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between bg-black relative">
-            <Image
-                src="/background.svg"
-                alt="Background"
-                fill
-                style={{ objectFit: 'cover' }}
-                priority
-            />
+        <main className="flex min-h-screen flex-col items-center justify-between bg-custom-dark relative">
             <Navigation />
-            <div className="z-10 flex flex-col items-center justify-center gap-8">
+
+            <div
+                className="z-10 flex flex-col items-center justify-center gap-8"
+                style={{
+                    transform: `translateY(-100px)`,
+                }}
+            >
+                <div className="relative">
+                    <Image
+                        src="/title.svg"
+                        alt="Silhouette Title"
+                        width={916}
+                        height={1000}
+                        priority
+                    />
+                </div>
+                <Image
+                    src="/coming_soon.svg"
+                    alt="Coming Soon"
+                    width={320}
+                    height={81}
+                    priority
+                />
+            </div>
+
+            {/* <div className="z-10 flex flex-col items-center justify-start gap-20">
                 <Image
                     src="/title.svg"
                     alt="Silhouette Title"
@@ -28,8 +46,22 @@ export default function Home() {
                     height={81}
                     priority
                 />
-            </div>
+            </div> */}
+
             <Footer />
+
+            {/* Background Image Container */}
+            <div className="absolute bottom-0 left-0 w-full h-1/2 overflow-hidden z-0">
+                <Image
+                    src="/background.svg"
+                    alt="Background"
+                    fill
+                    style={{ objectFit: 'cover' }}
+                    priority
+                />
+            </div>
+            {/* Gradient Overlay for Smooth Transition */}
+            <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-black to-transparent z-[-1]" />
         </main>
     )
 }
